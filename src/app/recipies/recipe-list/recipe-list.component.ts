f61d965b8a35cafd16e9be559debafdb535f94d2
Sourab2021 <88597457+Sourab2021@users.noreply.github.com>
2022-02-19 17:25:06 +0530

    recipecomponents

---
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from  '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipies: Recipe[] = [
+    new Recipe("trial", "fresh product", "https://media.istockphoto.com/photos/colorful-vegetables-and-fruits-vegan-food-in-rainbow-colors-picture-id1284690585?b=1&k=20&m=1284690585&s=170667a&w=0&h=HlEPBNsYMVuu-SsohPliBWHJy-IhW9y-fl8dS9KnBBo=" )
+
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
